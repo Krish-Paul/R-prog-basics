418b3f5d153f323b68fecc4c742568b4419ab035
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-06-26 08:07:55 +0530

    Create ifelse.R

---
+++ ifelse.R
@@ -0,0 +1,9 @@
+x=25
+if (is.integer(x))
+{
+  print("x is integer")
+}else
+{
+  print("x not integer")
+}
+#else will have to start for the line where if bracket is ending. if else starts from next line will show  error  
