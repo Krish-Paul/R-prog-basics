588d6a77341b131a87547d04639f6bee83803e5a
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-06-26 08:14:37 +0530

    Update ifelse.R

--- ifelse.R
+++ ifelse.R
@@ -7,3 +7,13 @@ if (is.integer(x))
   print("x not integer")
 }
 #else will have to start for the line where if bracket is ending. if else starts from next line will show  error  
+
+
+y=c("ab","cd","ef","gh")
+if("gh" %in% y) # this is how you search in vector. syntax --> element %in% vector
+{
+  print("present")
+}else
+{
+  print("absent")
+}
