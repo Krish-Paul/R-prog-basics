1ccfd18181f1799895fa8848f4fb5abe9e6ef6cf
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-06-26 08:47:07 +0530

    Create switchcase.R

---
+++ switchcase.R
@@ -0,0 +1,15 @@
+x=switch(3,"ab","cd","ef","gh")
+print(x)
+# type 1 swtich --> switch(expression value,case 1 value,cas2 value.....)
+
+
+
+y<-"4"
+z<-switch(y,
+          "4"="Ram",
+        "14"="shayam",
+        "20"="Mohan",
+         "25"="Sumit"
+)
+print(z)
+#type 2 swirhc -->   matching 
