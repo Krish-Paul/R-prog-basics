d95c800e0ba4e3665c32d39ced9f6de45f968b2c
Krish-Paul <115483010+Krish-Paul@users.noreply.github.com>
2024-06-28 07:44:10 +0530

    Update arithmetic_operator.R

--- arithmetic_operator.R
+++ arithmetic_operator.R
@@ -1,18 +1,1 @@
-a<-7.5
-b<-24
 
-print(a+b) #Addition
-print(a-b) #subtraction
-print(a*b) #multiplication
-print(a/b) #division
-print(a%%b) #remainder
-print(a%/%b) #quotient
-
-c1=c(8,9,6)#vector is initialised like this
-c2=c(2,4,5)
-print(c1+c2) #addition
-print(c1-c2) #subtraction
-print(c1*c2) #multiplication
-print(c1/c2) #division
-print(c1%%c2) #reminder
-print(c1%/%c2) #quotient
